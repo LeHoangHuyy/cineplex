85464bf4ea166ea713095c73355a4bb6e201885b
LeHoangHuyy <perconmy3n@gmail.com>
2026-09-09 17:01:16 +0700

    style(admin): expand main admin content area to full width

--- frontend/src/pages/admin/AdminLayout.tsx
+++ frontend/src/pages/admin/AdminLayout.tsx
@@ -246,7 +246,7 @@ export const AdminLayout: React.FC = () => {
       </aside>
 
       {/* Main Admin Content Area */}
-      <main className="flex-1 p-6 sm:p-8 lg:p-10 min-w-0 max-w-7xl">
+      <main className="flex-1 p-6 sm:p-8 lg:p-10 min-w-0 w-full">
         <Outlet />
       </main>
     </div>
